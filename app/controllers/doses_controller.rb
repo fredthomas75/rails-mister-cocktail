ddbc313d62312d588bd7118a2987aa01ae2597af
Frederic Thomas <fred@mt2lab.com>
2019-08-16 14:48:01 -0400

    Version 2

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,12 @@
 class DosesController < ApplicationController
+
+  before_action :set_cocktail, except: [:destroy]
+
   def new
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new
   end
 
   def create
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
     @dose.cocktail = @cocktail
     if @dose.save
@@ -18,11 +19,17 @@ class DosesController < ApplicationController
   def destroy
     @dose = Dose.find(params[:id])
     @dose.destroy
+    redirect_to cocktail_path(@dose.cocktail_id)
   end
 
   private
 
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
   def dose_params
     params.require(:dose).permit(:description, :ingredient_id)
   end
+
 end
